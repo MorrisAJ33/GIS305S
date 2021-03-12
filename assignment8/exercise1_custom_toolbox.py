163997f51f423d944914d5a0eec1339cda278b27
morri <morrisaj18@gmail.com>
2021-03-11 19:28:10 -0700

    AM_Ex2

--- assignment8/exercise1_custom_toolbox.py
+++ assignment8/exercise1_custom_toolbox.py
@@ -1,5 +1,6 @@
 import arcpy
 
+#to alex morris modelbuilder .gdb
 
 def intersect(layer_list, input_lyr_name):
 
@@ -15,7 +16,7 @@ def buffer_layer(input_gdb, input_layer, dist):
     units = " miles"
     dist = dist + units
     # Output layer will always be named input layer + "_buf
-    output_layer = r"C:\Users\David Neufeld\Documents\ArcGIS\GIS305\Projects\ModelBuilder\ModelBuilder.gdb\\" + input_layer + "_buf"
+    output_layer = r"C:\Users\morri\Documents\Alex Morris\GIS 305\Projects\GIS305_1\modelbuilder.gdb\\" + input_layer + "_buf"
     # Always use buffer parameters FULL, ROUND, ALL
     buf_layer = input_gdb + input_layer
     arcpy.Buffer_analysis(buf_layer, output_layer,
@@ -25,14 +26,14 @@ def buffer_layer(input_gdb, input_layer, dist):
 
 def main():
     # Define your workspace and point it at the modelbuilder.gdb
-    arcpy.env.workspace = r"C:\Users\David Neufeld\Documents\ArcGIS\GIS305\Projects\ModelBuilder\ModelBuilder.gdb\\"
+    arcpy.env.workspace = r"C:\Users\morri\Documents\Alex Morris\GIS 305\Projects\GIS305_1\modelbuilder.gdb\\"
     arcpy.env.overwriteOutput = True
 
     # Buffer cities
-    input_gdb = r"C:\Users\David Neufeld\Documents\ArcGIS\GIS305\Data\Admin\AdminData.gdb\USA\\"
+    input_gdb = r"C:\Users\morri\Documents\Alex Morris\GIS 305\Data\AdminData.gdb\USA\\"
 
     # Change me this next line below to use GetParamters!!
-    dist = input("What buffer distance do you want to use?")
+    dist = arcpy.GetParameterAsText(0)
 
     buf_cities = buffer_layer(input_gdb, "cities", dist)
 
@@ -41,7 +42,7 @@ def main():
 
     # Buffer rivers
     # Change me this next line below to use GetParamters!!
-    dist = input("What buffer distance do you want to use?")
+    dist = arcpy.GetParameterAsText(1)
     buf_rivers = buffer_layer(input_gdb, "us_rivers", dist)
     print("Buffer layer " + buf_rivers + " created.")
 
@@ -49,17 +50,15 @@ def main():
     # with names of input layers to intersect
     # Ask the user to define an output layer name
     # Change me this next line below to use GetParamters!!
-    intersect_lyr_name = input("What is the name for your output layer resulting from the intersect analysis? ")
+    intersect_lyr_name = arcpy.GetParameterAsText(2)
     lyr_list = [buf_rivers, buf_cities]
     intersect(lyr_list, intersect_lyr_name)
     print(f"New intersect layer generated called: {intersect_lyr_name}")
 
     # Get the project
-    aprx = arcpy.mp.ArcGISProject(
-        r"c:\Users\David Neufeld\Documents\ArcGIS\GIS305\Projects\ModelBuilder\ModelBuilder.ap"
-        r"rx")
+    aprx = arcpy.mp.ArcGISProject(r"C:\Users\morri\Documents\Alex Morris\GIS 305\Projects\GIS305_1\GIS305_1.aprx")
     map_doc = aprx.listMaps()[0]
-    map_doc.addDataFromPath(rf"C:\Users\David Neufeld\Documents\ArcGIS\GIS305\Projects\ModelBuilder\ModelBuilder.gdb\{intersect_lyr_name}")
+    map_doc.addDataFromPath(rf"C:\Users\morri\Documents\Alex Morris\GIS 305\Projects\GIS305_1\modelbuilder.gdb\{intersect_lyr_name}")
 
     aprx.save()
 
