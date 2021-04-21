7c0e0f8a52cb40832f1f74043dd7a8b240fec491
morri <morrisaj18@gmail.com>
2021-04-21 17:28:14 -0600

    lab2

--- lab2.py
+++ lab2.py
@@ -51,12 +51,11 @@ def mapthis(out_layer):
 def setup():
     with open('config/wnvoutbreak.yaml') as f:
         config_dict = yaml.load(f, Loader=yaml.FullLoader)
-    logging.basicConfig(filename=f"{config_dict.get('proj_dir')}wnv.log", filemode="w", level=logging.DEBUG)
     return config_dict
 def etl():
     GSheetsEtl(config_dict)
-    #etl_instance = GSheetsEtl(config_dict)
-    #etl_instance.process()
+    etl_instance = GSheetsEtl(config_dict)
+    etl_instance.process()
 
 if __name__ == '__main__':
     global config_dict
